28ec9d2dd22c080710244bae330feaa423babef4
Scott Grobe <Gitgrobe@gmail.com>
2019-11-24 20:48:10 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import ApiCall from './components/ApiCall'
+
+import './App.css';
+
+const App = (props) => {
+  return (
+    <>
+    <ApiCall />
+    </>
+  );
+}
+
+
+export default App;
